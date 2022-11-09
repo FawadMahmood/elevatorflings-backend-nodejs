ad2251ee0a39d2fe6cde6b1482192b385fbc85a9
Fawad Mahmood Siddiqui <fawad.mahmood@outlook.com>
2022-11-09 05:34:46 +0500

    added feeds filters

--- src/controllers/feed.controller.ts
+++ src/controllers/feed.controller.ts
@@ -9,34 +9,65 @@ const User: mongoose.Model<any> = require('../models/users');
 export class FeedController {
     @VerifyAuthorization
     async getFeeds(args: any, ctx: Context) {
-        const { first, cursor, distance } = args;
+        const { first, cursor, distance, filters, interests } = args;
         const user = await User.findById(ctx._id);
-        const feeds = await Feed.find({
-            $and: [
-                {
-                    location: {
-                        $near:
-                        {
-                            $geometry: user.location,
-                            $minDistance: 0,
-                            $maxDistance: distance ? distance : 1000
-                        }
+
+
+        let conditions: any[] = [
+            {
+                location: {
+                    $near:
+                    {
+                        $geometry: user.location,
+                        $minDistance: 0,
+                        $maxDistance: distance ? distance : 1000
                     }
-                },
-                {
-                    user: ctx._id
-                },
-                {
-                    ignored: false
-                },
-                {
-                    match: false,
-                },
-                cursor ? {
-                    _id: { $gt: new mongoose.Types.ObjectId(cursor) }
-                } : {}
-            ],
-        }).limit(first ? first : 5).populate('interests', '_id title addedBy').populate('ref_user');
+                }
+            },
+            {
+                user: ctx._id
+            },
+            {
+                ignored: false
+            },
+            {
+                match: false,
+            },
+            cursor ? {
+                _id: { $gt: new mongoose.Types.ObjectId(cursor) }
+            } : {}
+        ];
+
+        console.log("interests", interests);
+
+        if (filters) {
+            filters.map((_filter: { key: string, value: string }) => {
+                conditions.push(
+                    {
+                        [_filter.key]: _filter.value
+                    }
+                )
+            })
+        }
+
+
+        if (interests) {
+            conditions.push(
+                { "interests": { $in: interests } }
+            )
+        }
+
+
+
+
+        let applied_filters = {
+            $and: [
+                ...conditions,
+            ]
+        };
+
+
+        const feeds = await Feed.find(applied_filters).limit(first ? first : 5).populate('interests', '_id title addedBy').populate('ref_user');
 
 
         return {
