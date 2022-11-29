1c12e2ec1bd1de820b9c2156a677eeadadb3ab3c
FawadMahmood <fawad.mahmood@outlook.com>
2022-11-29 03:05:38 -0800

    changed connection to live server staging

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export abstract class Config {
   // public static mongoUrl = `mongodb+srv://${process.env.mongo_username}:${process.env.mongo_password}@${process.env.mongo_host_name}/${process.env.mongo_db_name}?retryWrites=true&w=majority`;
-  public static mongoUrl = `mongodb://localhost:27017/elevator`
-  // public static mongoUrl=`mongodb://db_user_elevatorflings:Binate%401234@server.appsstaging.com:27017/elevatorflings?authSource=elevatorflings`
+  // public static mongoUrl = `mongodb://localhost:27017/elevator`
+  public static mongoUrl=`mongodb://db_user_elevatorflings:Binate%401234@server.appsstaging.com:27017/elevatorflings?authSource=elevatorflings`
 }
 
